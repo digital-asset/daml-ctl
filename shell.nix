b3cb00812f8f318115c21ee1ce34173f66a8081c
markus-da <103116047+markus-da@users.noreply.github.com>
2025-02-13 10:30:53 +0100

    Install daml from Artifactory if not available on GitHub (#23)
    * update sdk and bump version
    
    * set build-options --target=1.17
    
    * get daml using nix from github or artifactory
    
    * clean up
    
    * set up nix in circle ci
    
    * run setup_nix in circle ci
    
    * daml build using nix in ci

---
+++ shell.nix
@@ -0,0 +1,44 @@
+# Add new nixpkgs source with (find matching revision here https://lazamar.co.uk/nix-versions/):
+# nix-shell -p niv --run "niv add NixOS/nixpkgs -n nixpkgs-ghc8107 -b master -r d1c3fea7ecbed758168787fe4e4a3157e52bc808"
+# Update nixpkgs with:
+# nix-shell -p niv --run "niv update"
+
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+  pkgsGhc = import sources.nixpkgs-ghc8107 {};
+  build_daml = import ./nix/daml.nix;
+  damlYaml = builtins.fromJSON (builtins.readFile (pkgs.runCommand "daml.yaml.json" { yamlFile = ./daml.yaml; } ''
+                ${pkgs.yj}/bin/yj < "$yamlFile" > $out
+              ''));
+  daml = (build_daml { stdenv = pkgs.stdenv;
+                       jdk = pkgs.openjdk11_headless;
+                       sdkVersion = damlYaml.sdk-version;
+                       damlVersion = damlYaml.daml-version;
+                       tarPath = damlYaml.daml-tar-path or null;
+                       curl = pkgs.curl;
+                       curl_cert = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+                       os = if pkgs.stdenv.isDarwin then "macos" else "linux";
+                       osJFrog = if pkgs.stdenv.isDarwin then "macos" else "linux-intel";
+                       hashes = { linux = "ii5kHI47ZH0Pq9BrPuMmy6vmXaZtnKZ5tHq4pnqJkt8=";
+                                  macos = "vWilCmSv0pp46Q5Lqkil0/V8c95x+gDNvlooh3w+ibU="; };});
+in
+pkgs.mkShell {
+  SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+  buildInputs = [
+    daml
+    pkgs.bash
+    pkgs.binutils # cp, grep, etc.
+    pkgs.cacert
+    pkgs.circleci-cli
+    pkgs.curl
+    pkgs.gh
+    pkgs.git
+    pkgs.gnupg
+    pkgs.jq
+    pkgs.python39
+    pkgs.openssh
+    pkgs.unixtools.xxd
+    pkgs.yq-go
+  ];
+}
