0cf6e00c0c13adbf491b895419b35862b43d5262
karen-staner-da <karen.staner@digitalasset.com>
2025-12-08 10:41:23 +0100

    Karen sdk3.4.x - daml-ctl SDK 3.4.9 (#34)
    * daml-control: data-dependencies
    
    * daml-control: chnaged Hash
    
    * build: set LF target to 2.dev in daml.yaml
    
    * align daml-ctl with SDK 3.4.0-snapshot.20251002.0
    
    * Changed linux hash
    
    * upgrade to SDK 3.4.0-snapshot.20251006.0
    
    * update shell.nix
    
    * Changed version
    
    * Rebuild daml-ctl DAR with SDK 3.4.0-snapshot.20251006.1457.29a2bd8 - match DPM
    
    * Changed SDK 3.4.0-snapshot.20251006.1457.29a2bd8 to match DPM - changed hash
    
    * Upgrade daml-ctl to SDK 3.4.0-snapshot.20251013.1566.e75a9cf
    
    * Changed Hash
    
    * Upgrade daml-ctl to SDK 3.4.0-snapshot.20251013.0
    
    * Upgrade daml-ctl to SDK 3.4.0-snapshot.20251013.0 - Update Linux Hash
    
    * Upgrade daml-ctl to SDK 3.4.0-rc2
    
    * Upgrade daml-ctl to SDK 3.4.0-rc2 - Final
    
    * New SKD 3.4.7
    
    * Upgrade SDK 3.4.7
    
    * Update daml-ctl to SDK 3.4.8
    
    * Update daml-ctl to SDK 3.4.8
    
    * daml-ctl: SDK 3.4.9
    
    * daml-ctl: SDK 3.4.9

--- shell.nix
+++ shell.nix
@@ -29,8 +29,8 @@ let
                        os = os;
                        arch = arch;
                        osJFrog = if pkgs.stdenv.isDarwin then "macos" else "linux-intel";
-                       hashes = { linux = "Kt3UHV6Md/tc7e2g7f0icgf7CsFvhwi6xHSXFxBzan4=";
-                                  macos = "PEhY2JewOXERJ5BkwmIl0ikgnQ6ttR7OlpE3zg0Pw1A="; };});
+                       hashes = { linux = "zPPJJfor22GHpovh2HOJH7AKQLfSW9p0UPgcZCdhSGM=";
+                                  macos = "hITo4qlasMbhuLGfUwGMhuvkwVRaNgWQLdl6mEDx2Ew="; };});
 in
 pkgs.mkShell {
   SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
